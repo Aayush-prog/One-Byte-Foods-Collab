df11a9afcfae0a7f4512d2c53c1a52ec5ea6e9cf
Krishma-a <np03cs4a220060@heraldcollege.edu.np>
2024-05-03 21:46:57 +0545

    splash screen timer

---
+++ lib/nav/landingpage.dart
@@ -0,0 +1,81 @@
+// import 'package:flutter/material.dart';
+// import 'package:one_byte_foods/nav/homepage.dart';
+// import 'package:one_byte_foods/nav/navigatorpage.dart';
+
+// class LandingPage extends StatelessWidget {
+//   const LandingPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+//       const SizedBox(height: 100),
+//       Column(
+//         children: [
+//           Image.asset("assets/images/logo.png"),
+//           const Text("One Byte Foods",
+//               style: TextStyle(
+//                 fontWeight: FontWeight.bold,
+//                 fontSize: 30,
+//               )),
+//           const Text("Where Every Flavor Tells A Story",
+//               style: TextStyle(fontWeight: FontWeight.bold)),
+//         ],
+//       ),
+//       const SizedBox(height: 300),
+//       ElevatedButton(
+//           onPressed: () {
+//             print("pressed");
+//             Navigator.push(context, MaterialPageRoute(builder: (context) {
+//               print("inside route");
+//               return MyWidget();
+//             }));
+//           },
+//           child: const Text("Let's Get Started")),
+//     ]);
+//   }
+// }
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:one_byte_foods/main.dart';
+import 'package:one_byte_foods/nav/navigatorpage.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  void initState() {
+    super.initState();
+    _navigate();
+  }
+
+  void _navigate() {
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => MyWidget()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Column(
+        children: [
+          Image.asset("assets/images/logo.png"),
+          const Text("One Byte Foods",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              )),
+          const Text("Where Every Flavor Tells A Story",
+              style: TextStyle(fontWeight: FontWeight.bold)),
+        ],
+      ),
+    ]);
+  }
+}
